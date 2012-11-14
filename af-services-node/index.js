08e7779f99dd8fac10a248ce7eef45de5b232b6c
srosh <srosh@me.com>
2012-11-13 22:45:09 -0800

    adding pg,mysql and mongodb

--- af-services-node/index.js
+++ af-services-node/index.js
@@ -28,5 +28,68 @@ for (key in appFogServices)
 			}
 		}
 	};
+	if (key.match(/mysql/))
+	{
+		services.mysql={};
+		getall(appFogServices[key],services.mysql);
+		for (name in services.mysql)
+		{
+			services.mysql[name].connect = function (db,connectionListener) {
+				var mysqlModule = require("mysql-native");
+				if (!mysqlModule) return null;
+				client = mysqlModule.createTCPClient(this.cred.host,this.cred.port,connectionListener);
+				client.auto_prepare = true;
+				client.auth(db,this.cred.username,this.cred.password);
+			}
+		}
+	};
+	if (key.match(/postgresql/))
+	{
+		services.pgsql={};
+		getall(appFogServices[key],services.pgsql);
+		for (name in services.pgsql)
+		{
+			services.pgsql[name].connect = function (db,options) {
+				var pgsqlModule = require('pg');
+				if (!pgsqlModule) return null;
+				if (!options.database) options.database = db;
+				options.user = this.cred.user;
+				options.password = this.cred.password;
+				options.port = this.cred.port;
+				options.host = this.cred.host;
+				client = new pgsqlModule.Client(options);
+				client.connect();
+				return client;
+			}
+		}
+	};
+	if (key.match(/mongodb/))
+	{
+		services.mongodb={};
+		getall(appFogServices[key],services.mongodb);
+		for (name in services.mongodb)
+		{
+			services.mongodb[name].connect = function (db,options) {
+				var url = 'mongodb://' + this.cred.username + ':' + this.cred.password + '@' + this.cred.hostname + ':' + this.cred.port + '/' + this.cred.db ;
+				var mongodbModule=require('mongodb');
+				if (!mongodbModule) return null;
+				mongodbModule.connect(url ,options , function(error, client){
+					if (error != null) return null;
+					return client;
+				});
+			}
+		}
+	};
+	if (key.match(/rabbitmq/))
+	{
+		services.rmq={};
+		getall(appFogServices[key],services.rmq);
+		for (name in services.rmq)
+		{
+			services.rmq[name].connect = function (options) {
+				return null;
+			}
+		}
+	};
 }
 module.exports = services;
